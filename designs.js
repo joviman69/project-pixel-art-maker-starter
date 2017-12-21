cecc4532156f35c83d445680fb32c374d1477797
Jose Contamina <joviman69@gmail.com>
2017-12-21 19:45:18 +0100

    Corregidos errores

--- designs.js
+++ designs.js
@@ -1,10 +1,9 @@
 var color;
-var colorRight;
-var height;
-var width;
+var height = 20 ;
+var width = 20;
 
 
-/// Set color val
+/// Set color value
 $('#colorPicker').change(function() {
     color = $(this).val();
 });
@@ -24,11 +23,11 @@ $('#sizePicker').submit(function(event) {
     var grid = makeGrid(width, height);
     event.preventDefault();
 });
-///Grid reset
+/// Grid reset
 $('#reset').on('click', function() {
     $('td').css('background-color', '');
 });
-////makeGrid
+/// makeGrid
 
 function makeGrid(width, height) {
     var grid = $('#pixel_canvas');
@@ -36,8 +35,8 @@ function makeGrid(width, height) {
 
     for (var rows = 1; rows <= Number(height); rows++) {
         var tr = $('<tr class="rows">');
-        for (var col = 1; col <= Number(width); col++) {
-            var td = tr.append('<td class="cell">')
+        for (var cols = 1; cols <= Number(width); cols++) {
+            var td = tr.append('<td class="cell">');
         }
         grid.append(tr);
     }
@@ -51,32 +50,10 @@ $(document).on('mousedown mouseup', 'td', function mouseState(e) {
             let color = $('#colorPicker').val();
             $(this).css('background-color', color);
         }
-        if (e.which === 2) {
+        if (e.which === 3) {
             $(this).css('background-color', '#ffffff');
             e.preventDefault();
 
         }
     }
-});
-
-//hold drawing
-$('body').mousedown(function(e) {
-    if (e.which === 1) {
-        $('td').bind('hover', function() {
-            let color = $('#colorPicker').val();
-            $(this).css('background-color', color);
-        });
-    }
-    if (e.which === 2) {
-        $('td').bind('hover', function() {
-            $(this).css('background-color', '#ffffff');
-        });
-    }
-
-});
-$('body').mouseup(function() {
-    $("body td").unbind("hover");
-    $("body").css(function() {
-        return false;
-    });
 });
